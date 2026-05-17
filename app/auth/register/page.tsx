1e8f845ceb8179d64660e198b5cdbe7c54db3a25
Tomas Urbano <toomas.urbano14@gmail.com>
2026-05-16 22:38:21 -0300

    fix temporal

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -105,3 +105,7 @@ export default function RegisterPage() {
     </motion.div>
   );
 }
+
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("ANON:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
